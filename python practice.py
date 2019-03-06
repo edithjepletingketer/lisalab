b0a6bbac473ce6a33f5b4c7dff37feea1fbf7570
edithjepletingketer <edithjepleting@gmail.com>
2019-03-06 04:06:31 -0800

    i have added a files.

---
+++ python practice.py
@@ -0,0 +1,116 @@
+=[Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5+7
+12
+>>> 1000000+10000000
+11000000
+>>> 13-8
+5
+>>> 3*3
+9
+>>> 15/3
+5.0
+>>> 10/3
+3.3333333333333335
+>>> 10//3
+3
+>>> 10%3
+1
+>>> 20%3
+2
+>>> 1000%17
+14
+>>> 25%7
+4
+>>> 2**2
+4
+>>> 2**3
+8
+>>> (2+2)*3/4+5-2
+6.0
+>>> 2==2
+True
+>>> 2==3
+False
+>>> 4387==4387
+True
+>>> 2!=2
+False
+>>> 21=3
+SyntaxError: can't assign to literal
+>>> 2!=3
+True
+>>> 2<3
+True
+>>> 2<2
+False
+>>> 2<=3
+True
+>>> 2<=2
+True
+>>> 2<=1
+False
+>>> 5>4
+True
+>>> 5>6
+False
+>>> 5>=4
+True
+>>> 7>=8
+False
+>>> 7>=7
+True
+>>> 17*23,18*22
+(391, 396)
+>>> 17*23<18*22
+True
+>>> 33//7>34//6
+False
+>>> a=a
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    a=a
+NameError: name 'a' is not defined
+>>> "a"="a"
+SyntaxError: can't assign to literal
+>>> "a"
+'a'
+>>> 'a'
+'a'
+>>> "a'
+SyntaxError: EOL while scanning string literal
+>>> "a"=="a"
+True
+>>> "a"=="b"
+False
+>>> "boy"=="girl"
+False
+>>> "a"+"a"
+'aa'
+>>> "aa"+'b
+SyntaxError: EOL while scanning string literal
+>>> "aa"+"bbb"+"ccc"
+'aabbbccc'
+>>> "irene"+'mwangi"
+SyntaxError: EOL while scanning string literal
+>>> "irene"+"mwangi"
+'irenemwangi'
+>>> "irene"+20
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    "irene"+20
+TypeError: can only concatenate str (not "int") to str
+>>> "b"-"a"
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    "b"-"a"
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+>>> "a"*10
+'aaaaaaaaaa'
+>>> "a">="b"
+False
+>>> "b">="a"
+True
+>>> "ab"<"bc"
+True
+>>> 
